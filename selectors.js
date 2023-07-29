b462f7655d6b58348aadeda97d830358fc7c199a
vi xent <alagovincent03@gmailcom>
2023-07-29 05:18:18 -0700

    done

--- selectors.js
+++ selectors.js
@@ -4,30 +4,54 @@ const select = () => {
     /* Section 1 */
     // 1. Get all seeded fruit elements
     // Your code here
-    
+     const seeded = document.querySelectorAll(".seed")
+       seeded.forEach(seed => {
+        console.log(seed)
+    })
+
     // 2. Get all seedless fruit elements
     // Your code here
+    const seedless = document.querySelectorAll(".seedless")
+    seedless.forEach(seedless => {
+        console.log(seedless)
+    })
 
     // 3. Get first seedless fruit element
     // Your code here
+    console.log(seedless[0])
 
     /* Section 2 */
     // 4. Get inner span with text "you"
     // Your code here
+   console.log( document.querySelector("span"))
 
     // 5. Get all children of element "wrapper"
     // Your code here
+   console.log( document.getElementById('#wrapper'))
 
     // 6. Get all odd number list items in the list
     // Your code here
+    const odd = document.querySelectorAll(".odd")
+    odd.forEach(odd =>{
+        console.log(odd)
+    })
 
     // 7. Get all even number list items in the list
     // Your code here
+     // Your code here
+     const even = document.querySelectorAll(".even")
+     even.forEach(even =>{
+         console.log(even)
+     })
 
     /* Section 3 */
     // 8. Get all tech companies without a class name
     // Your code here
-
+     // Your code here
+     const anchor = document.querySelectorAll("a")
+     anchor.forEach(a =>{
+         console.log(a)
+     })
     // 9. Get "Amazon" list element
     // Your code here
 
